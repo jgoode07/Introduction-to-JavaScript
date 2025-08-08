89ef1ea07ca7571f20fda61ba2e8bd920ca0e663
jgoode07 <joegoode.07@gmail.com>
2025-08-08 14:11:53 -0700

    Add program to calculate largest of 3 numbers
    confirmed in the browser console.

--- joe-goode-javascript-m1.2.js
+++ joe-goode-javascript-m1.2.js
@@ -19,3 +19,27 @@ let celsiusFromF = (fahrenheit - 32) * 5/9;
 
 // Output result using string concatenation and one decimal place
 console.log(fahrenheit.toFixed(1) + " Fahrenheit is " + celsiusFromF.toFixed(1) + " Celsius");
+
+
+// M1.2 Exercise 2: Largest of three numbers
+// Note: This doesn't check for duplicate numbers or handle cases where nothing is entered.
+
+// Ask the user for three numbers and store them as numbers
+let a = parseFloat(prompt("Enter the first number:", ""));
+let b = parseFloat(prompt("Enter the second number:", ""));
+let c = parseFloat(prompt("Enter the third number:", ""));
+
+// Assign the largest number to a variable
+let largest;
+
+// Compare the numbers using if-else statements and logical AND (&&) operators
+if (a > b && a > c) {
+    largest = a;
+} else if (b > a && b > c) {
+    largest = b;
+} else {
+    largest = c;
+}
+
+// Output result using string concatenation
+console.log("The largest number is " + largest);
